a9cd653b4e37cbee8e9a34a8c140fd2b12200e46
Chris Whelan <cwhelan@broadinstitute.org>
2019-07-02 13:35:26 -0400

    parse PQ as string

--- src/main/java/org/broadinstitute/hellbender/tools/walkers/variantutils/ExtractPhaseInformativeReads.java
+++ src/main/java/org/broadinstitute/hellbender/tools/walkers/variantutils/ExtractPhaseInformativeReads.java
@@ -226,7 +226,7 @@ public class ExtractPhaseInformativeReads extends VariantWalker {
         }
 
         public void addPhasedGT(final VariantContext vc, final Genotype genotype) {
-            final Tuple2<Boolean, Integer> value = new Tuple2<>(vc.getAlleleIndex(genotype.getAllele(0)) == 0, ((Number)  genotype.getAnyAttribute("PQ")).intValue());
+            final Tuple2<Boolean, Integer> value = new Tuple2<>(vc.getAlleleIndex(genotype.getAllele(0)) == 0, Integer.parseInt(genotype.getAnyAttribute("PQ").toString()));
             phasedGts.put(vc.getStart(), value);
         }
 
